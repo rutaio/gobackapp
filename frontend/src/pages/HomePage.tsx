24fe8ae430778b6863516fc4660b171d0ce4b6cd
“rutaiogit config --global user.email youremail@gmail.comrutadanyt@gmail.com <rutadanyt@gmail.com>
2025-12-17 19:31:11 +0200

    Add basic routing and HomePage

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,18 @@
+export const HomePage = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="left-column">
+          <h2>Threads</h2>
+          <p>Your project work areas</p>
+           <div>Threads list goes here</div>
+        </div>
+        <div className="right-column">
+          <h2>Go Back</h2>
+          <p>Continue from where you left off</p>
+          <div>Checkin card goes here</div>
+        </div>
+      </div>
+    </>
+  );
+};
