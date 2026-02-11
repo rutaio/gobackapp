71d7b83dd0a24b60d10ea08c756a2e92f9769d15
“rutaiogit config --global user.email youremail@gmail.comrutadanyt@gmail.com <rutadanyt@gmail.com>
2026-02-11 17:04:27 +0200

    add footer with app's name and description

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h2>Go Back App</h2>
+        <p>Return. Reflect. Continue.</p>
+      </div>
+      <div>
+        <p>
+          A quiet space for solo creators who juggle many projects. Remember
+          where you left off and take one small step forward.
+        </p>
+      </div>
+    </footer>
+  );
+};
