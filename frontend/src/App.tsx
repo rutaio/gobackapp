81a5d927cc6432b5e849ac97324507d7f865c78e
Ruta Jakute <rutajakute@gmail.com>
2026-02-18 21:07:15 +0200

    feat: add About page (#79)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
+import { AboutPage } from './pages/AboutPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
     </BrowserRouter>
   );
